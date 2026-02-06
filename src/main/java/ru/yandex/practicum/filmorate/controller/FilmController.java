4b9720ad253f89f569bc623953024a2c208def61
Andrey Chaykovskiy <andy.tchaikovsky@yandex.ru>
2026-02-05 21:26:14 -0500

    added all but tests

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,97 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            log.warn("Ошибка валидации фильма: Название не может быть пустым");
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (film.getDescription() == null || film.getDescription().length() > 200) {
+            log.warn("Ошибка валидации фильма: Максимальная длина описания — 200 символов");
+            throw new ValidationException("Максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            log.warn("Ошибка валидации фильма: Дата релиза — не раньше 28 декабря 1895 года");
+            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() == null || film.getDuration().isZero() || film.getDuration().isNegative()) {
+            log.warn("Ошибка валидации фильма: Продолжительность фильма должна быть положительным числом");
+            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+        }
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Добавлен фильм: {}", film.getName());
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            throw new ValidationException("Id должен быть указан");
+        }
+
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            if (newFilm.getName() == null || newFilm.getName().isBlank()) {
+                log.warn("Ошибка валидации фильма: Название не может быть пустым");
+                throw new ValidationException("Название не может быть пустым");
+            }
+            if (newFilm.getDescription() == null || newFilm.getDescription().length() > 200) {
+                log.warn("Ошибка валидации фильма: Максимальная длина описания — 200 символов");
+                throw new ValidationException("Максимальная длина описания — 200 символов");
+            }
+            if (newFilm.getReleaseDate() == null || newFilm.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+                log.warn("Ошибка валидации фильма: Дата релиза — не раньше 28 декабря 1895 года");
+                throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
+            }
+            if (newFilm.getDuration() == null || newFilm.getDuration().isZero() ||newFilm.getDuration().isNegative()) {
+                log.warn("Ошибка валидации фильма: Продолжительность фильма должна быть положительным числом");
+                throw new ValidationException("Продолжительность фильма должна быть положительным числом");
+            }
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+            log.info("Обновлён фильм с id={}", newFilm.getId());
+            return oldFilm;
+        }
+        throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
+    }
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        return films.values();
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+
+
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,18 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    Long id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Duration duration;
 }
